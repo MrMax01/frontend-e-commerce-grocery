ff915ed38e4afbd77a8028462d01894aa15084c6
MrMax01 <massimo.mauro00@gmail.com>
2023-12-20 16:33:59 +0100

    HomePage init

--- src/componets/HomePage.jsx
+++ src/componets/HomePage.jsx
@@ -1,20 +1,37 @@
-import { Card, Container } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import headerImg from "../assets/headerImg.png";
+import vegetebles from "../assets/verdure.png";
+import fish from "../assets/pesci.png";
 
 const HomePage = () => {
   return (
     <Container>
-      <Card className="bg-dark text-white">
-        <Card.Img src={headerImg} alt="Card image" height="500" className="" />
-        <Card.ImgOverlay>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in to additional content. This content is
-            a little bit longer.
-          </Card.Text>
-          <Card.Text>Last updated 3 mins ago</Card.Text>
+      <Card className="bg-dark text-white mb-5">
+        <Card.Img src={headerImg} alt="Card image" height="500" className="object-fit-cover" />
+        <Card.ImgOverlay className="d-flex justify-content-center align-items-center">
+          <Button variant="primary" className="mt-5">
+            Inizia ad acquistare
+          </Button>
         </Card.ImgOverlay>
       </Card>
+      <Row>
+        <Col>
+          <Card className="bg-dark text-white">
+            <Card.Img src={vegetebles} alt="Card image" height="500" className="object-fit-cover" />
+            <Card.ImgOverlay className="d-flex justify-content-center align-items-center">
+              <Card.Title className="h-1">PROMO #1</Card.Title>
+            </Card.ImgOverlay>
+          </Card>
+        </Col>
+        <Col>
+          <Card className="bg-dark text-white">
+            <Card.Img src={fish} alt="Card image" height="500" className="object-fit-cover" />
+            <Card.ImgOverlay className="d-flex justify-content-center align-items-center">
+              <Card.Title className="h-1">PROMO #2</Card.Title>
+            </Card.ImgOverlay>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 };
