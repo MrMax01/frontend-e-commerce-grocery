2b5280fe3f1a19ec033f7ec2ab65c348d8971c23
MrMax01 <massimo.mauro00@gmail.com>
2024-01-09 16:41:01 +0100

    style cart

--- src/componets/MyCart.jsx
+++ src/componets/MyCart.jsx
@@ -1,4 +1,4 @@
-import { Button, Card, Col, Container, Form, Modal, Row } from "react-bootstrap";
+import { Alert, Button, Card, Col, Container, Form, Modal, Row } from "react-bootstrap";
 import NavBar from "./NavBar";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
@@ -61,101 +61,131 @@ const MyCart = () => {
   return myToken != null ? (
     <Container>
       <NavBar />
-      <h2>Carrello:</h2>
-      <Row xs={1} lg={2} className="mt-5">
+      <h2 className="mt-5">Carrello:</h2>
+      <Row xs={1} lg={2}>
         {cartItems.length === 0 ? (
           <>
             <Col>
               <div className="d-flex justify-content-center align-items-center  border">
-                <h1>Carrello Vuoto</h1>
+                <i className="bi bi-cart-x fs-1 secondary-color me-2"></i>
+                <span className="fs-1">Carrello Vuoto</span>
               </div>
             </Col>
-            <Col></Col>
+            <Col className="text-center">
+              <h3>
+                TOTALE: <br />$ 0
+              </h3>
+            </Col>
           </>
         ) : (
           <>
             <Col>
+              <Row>
+                <Col>
+                  <span className="fw-bold">Item:</span>
+                </Col>
+                <Col>
+                  <span className="fw-bold">Quantità:</span>
+                </Col>
+                <Col>
+                  <span className="fw-bold">Costo:</span>
+                </Col>
+              </Row>
+              <hr />
               {cartItems.length > 0 &&
                 cartItems.map((cart, i) => (
-                  <div className="d-flex justify-content-between align-items-center mb-3 shadow-sm" key={cart.id}>
-                    <Card>
-                      <Card.Img variant="top" src={cart.product.photo} />
-                    </Card>
-                    <div>
-                      <p className="m-0">{cart.product.name}</p>
-                      <p className="m-0">{cart.product.category}</p>
-                      <p className="m-0">max: {cart.product.quantity}kg</p>
-                    </div>
+                  <div key={cart.id}>
+                    <Row className=" justify-content-between align-items-center mb-3 shadow-sm">
+                      <Col>
+                        <Card>
+                          <Card.Img variant="top" src={cart.product.photo} />
+                        </Card>
+                      </Col>
+                      <Col className="text-truncate">
+                        <div>
+                          <p className="m-0 ">{cart.product.name}</p>
+                          <p className="m-0 ">{cart.product.category}</p>
+                          <p className="m-0 ">max: {cart.product.quantity}kg</p>
+                        </div>
+                      </Col>
+                      <Col></Col>
+                      <Col>
+                        <div className="d-flex align-items-center">
+                          <div className="me-2">
+                            <span>{cart.quantity}kg</span>
+                          </div>
+                          <div>
+                            <i className="bi bi-gear-fill hvr-grow" onClick={() => handleShow(cart.id)}></i>
 
-                    <div className="d-flex align-items-center">
-                      <div className="me-2">
-                        <span>{cart.quantity}kg</span>
-                      </div>
-                      <div>
-                        <i className="bi bi-gear-fill" onClick={() => handleShow(cart.id)}></i>
-
-                        <Modal show={showModal === cart.id} onHide={handleClose}>
-                          <Modal.Header closeButton>
-                            <Modal.Title>{cart.product.category}</Modal.Title>
-                          </Modal.Header>
-                          <Modal.Body>
-                            <Form>
-                              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                                <Form.Label>quantità che hai bisogno(max: {cart.product.quantity}kg) </Form.Label>
-                                <div className="d-flex align-items-center mb-3">
-                                  <div className="me-2">
-                                    <Form.Control
-                                      type="number"
-                                      placeholder={cart.quantity}
-                                      onChange={(e) => {
-                                        setChangeQuantity({ id: cart.id, quantity: e.target.value });
-                                      }}
-                                    />
-                                  </div>
-                                  <div>
-                                    <span>kg</span>
-                                  </div>
-                                </div>
-                              </Form.Group>
-                            </Form>
-                          </Modal.Body>
-                          <Modal.Footer>
-                            <Button variant="secondary" onClick={handleClose}>
-                              Close
-                            </Button>
-                            <Button
-                              variant="primary"
-                              onClick={() => {
-                                handleSumbit();
-                              }}
-                            >
-                              Save Changes
-                            </Button>
-                          </Modal.Footer>
-                        </Modal>
-                      </div>
-                    </div>
-
-                    <span>${cart.product.unit_price * cart.quantity}</span>
-
-                    <i
-                      className="bi bi-x-lg"
-                      onClick={() => {
-                        dispatch(deleteMyCart(myToken, cart.id));
-                      }}
-                    ></i>
+                            <Modal show={showModal === cart.id} onHide={handleClose}>
+                              <Modal.Header closeButton>
+                                <Modal.Title>{cart.product.category}</Modal.Title>
+                              </Modal.Header>
+                              <Modal.Body>
+                                <Form>
+                                  <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                    <Form.Label>
+                                      cambia la quantità di cui hai bisogno(max: {cart.product.quantity}kg){" "}
+                                    </Form.Label>
+                                    <div className="d-flex align-items-center mb-3">
+                                      <div className="me-2">
+                                        <Form.Control
+                                          type="number"
+                                          placeholder={cart.quantity}
+                                          onChange={(e) => {
+                                            setChangeQuantity({ id: cart.id, quantity: e.target.value });
+                                          }}
+                                        />
+                                      </div>
+                                      <div>
+                                        <span>kg</span>
+                                      </div>
+                                    </div>
+                                  </Form.Group>
+                                </Form>
+                              </Modal.Body>
+                              <Modal.Footer>
+                                <Button variant="secondary" onClick={handleClose}>
+                                  Close
+                                </Button>
+                                <Button
+                                  variant="primary"
+                                  onClick={() => {
+                                    handleSumbit();
+                                  }}
+                                >
+                                  Save Changes
+                                </Button>
+                              </Modal.Footer>
+                            </Modal>
+                          </div>
+                        </div>
+                      </Col>
+                      <Col>
+                        <span>${cart.product.unit_price * cart.quantity}</span>
+                      </Col>
+                      <Col>
+                        <i
+                          className="bi bi-x-lg secondary-color fs-5 hvr-grow"
+                          onClick={() => {
+                            dispatch(deleteMyCart(myToken, cart.id));
+                          }}
+                        ></i>
+                      </Col>
+                    </Row>
+                    <Alert variant="danger" className={cart.product.quantity < cart.quantity ? "d-block" : "d-none"}>
+                      Attenzione rimettere la quantità che vuoi acquistare!
+                    </Alert>
                   </div>
                 ))}
             </Col>
             <Col className="text-center">
               <h3>
                 TOTALE: <br />$
-                {cartItems.length > 0
-                  ? cartItems.reduce(
-                      (totalQuantity, cartItem) => totalQuantity + cartItem.quantity * cartItem.product.unit_price,
-                      0
-                    )
-                  : 0}
+                {cartItems.reduce(
+                  (totalQuantity, cartItem) => totalQuantity + cartItem.quantity * cartItem.product.unit_price,
+                  0
+                )}
               </h3>
               <Button
                 onClick={() => {
@@ -173,22 +203,42 @@ const MyCart = () => {
         <h2>Ordini:</h2>
         {ordersItems.length === 0 ? (
           <div className="d-flex justify-content-center align-items-center  border">
-            <h1>Non hai ordini</h1>
+            <i className="bi bi-bag-x secondary-color fs-1 me-2"></i>
+            <span className="fs-1">Non hai ordini</span>
           </div>
         ) : (
-          ordersItems.map((order, i) => (
-            <Row key={order.id} className="shadow border mb-2">
-              <h5>ORDER ID: {order.id}</h5>
+          <>
+            <Row>
               <Col>
-                <Card style={{ width: "5rem" }}>
-                  <Card.Img variant="top" src={order.product.photo} />
-                </Card>
+                <span className="fw-bold">Item:</span>
               </Col>
-              <Col>{order.product.name}</Col>
-              <Col>{order.quantity}</Col>
-              <Col>{order.totalCost}</Col>
+              <Col>
+                <span className="fw-bold">Nome prodotto:</span>
+              </Col>
+              <Col>
+                <span className="fw-bold">stato dell'ordine:</span>
+              </Col>
+              <Col>
+                <span className="fw-bold">quantità:</span>
+              </Col>
+              <Col>
+                <span className="fw-bold">costo:</span>
+              </Col>
             </Row>
-          ))
+            {ordersItems.map((order, i) => (
+              <Row key={order.id} className="border  align-items-center mb-3">
+                <Col>
+                  <Card style={{ width: "5rem" }}>
+                    <Card.Img variant="top" src={order.product.photo} />
+                  </Card>
+                </Col>
+                <Col>{order.product.name}</Col>
+                <Col>{order.orderStatus}</Col>
+                <Col>{order.quantity}kg</Col>
+                <Col>{order.totalCost}$</Col>
+              </Row>
+            ))}
+          </>
         )}
       </Container>
     </Container>
